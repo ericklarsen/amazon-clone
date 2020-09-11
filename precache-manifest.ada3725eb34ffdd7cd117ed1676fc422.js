3ef74584bd48fa0170c00398fb775d1a2e5481b5
ericklarsen <ericklarsenn@gmail.com>
2020-09-11 17:51:24 +0700

    Updates

---
+++ precache-manifest.ada3725eb34ffdd7cd117ed1676fc422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30255478c76028b8fe270e6d1819a946",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "dbf4f4f64a22d43a55ab",
+    "url": "/amazon-clone/static/css/main.3b0a913f.chunk.css"
+  },
+  {
+    "revision": "903d8978897a38a3f95f",
+    "url": "/amazon-clone/static/js/2.9a94b490.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-clone/static/js/2.9a94b490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf4f4f64a22d43a55ab",
+    "url": "/amazon-clone/static/js/main.148897ee.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
